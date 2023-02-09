Rotation fix #2

packages/demo_package/src/controller_node.py
@@ -20,120 +20,144 @@ class ControllerNode(DTROS):
 
         # Publishers
         self.pub_wheel_commands = rospy.Publisher(f'/{self.veh_name}/wheels_driver_node/wheels_cmd', WheelsCmdStamped, queue_size=1)
-        
+
         # Services
         rospy.wait_for_service(f'/{self.veh_name}/led_emitter_node/set_pattern')
         self.led_service = rospy.ServiceProxy(f'/{self.veh_name}/led_emitter_node/set_pattern', ChangePattern)
-        
+
         # vehicle speeds
-        self.x_speed = 0.5
-        self.turn_speed = 0.3
-        
+        self.x_speed = 0.3
+        self.turn_speed = 0.45
+        self.slow_down = 0.15 # for slowing down turns during rotations
+        self.speed_up = 0.055 # for speeding up the lagging wheel
+
         # self.x_speed = 0.4
         # self.turn_speed = 0.15
-        
+
         # wheel distances
         self.distance_left = 0
         self.distance_right = 0
-        
-        self.l = 0.045 # meters between the center of the wheel and the robot rotation
-        self.r = 0.04	# radius of the wheel in meteres
 
-    
+        self.l = 0.05 # meters between the center of the wheel and the robot rotation
+        self.r = 0.033	# radius of the wheel in meteres
+
+
 
     def run(self):
+        self.first_right_turn(np.pi/2)
+        self.forward(1.25)
+        self.left_turn(np.pi/2)
+        self.forward(1.25)
+        self.left_turn(np.pi/2)
+        self.forward(1.25)
+        self.right_turn(np.pi/2)
         self.stop()
-        self.right_turn(np.pi/2)
-        self.forward(1.25)
-        self.left_turn(np.pi/2)
-        self.forward(1.25)
-        self.left_turn(np.pi/2)
-        self.forward(1.25)
-        self.right_turn(np.pi/2)
-        
+
         # self.left_turn(np.pi/2)
         # self.backward(0.3)
         return
-        
+
     def complete(self):
         self.publish_leds("WHITE")
-        
-    
+
+
     def forward(self, total_distance):
         starting_distance = (self.distance_left + self.distance_right) / 2
         current_distance = starting_distance
-        
+
         msg = WheelsCmdStamped()
         msg.header.stamp = rospy.get_rostime()
         msg.vel_left = self.x_speed
-        msg.vel_right = self.x_speed
-        
-        self.pub_wheel_commands.publish(msg)
-        
+        msg.vel_right = self.x_speed + self.speed_up # csc22906
+
         while current_distance - starting_distance < total_distance:
             current_distance = (self.distance_left + self.distance_right) / 2
             self.pub_wheel_commands.publish(msg)
-            
+
         self.stop()
-        
-        
+
+
     def backward(self, total_distance):
         starting_distance = (self.distance_left + self.distance_right) / 2
         current_distance = starting_distance
-        
+
         msg = WheelsCmdStamped()
         msg.header.stamp = rospy.get_rostime()
         msg.vel_left = -self.x_speed
         msg.vel_right = -self.x_speed
-	    
+
         rospy.loginfo("left:" + str(msg.vel_left) + " right: " + str(msg.vel_right))
-        self.pub_wheel_commands.publish(msg)
-        
+
         while starting_distance - current_distance < total_distance:
             current_distance = (self.distance_left + self.distance_right) / 2
             self.pub_wheel_commands.publish(msg)
-            
+
         self.stop()
-        
+
     def rot_dist(self, angle):
+        # angle is in radians
+        # r*theta is formula for arc length
         return angle * self.l
-     
+
     def left_turn(self, angle):
+        # total_distance is how far each wheel has to travel
         total_distance = self.rot_dist(angle)
 
         starting_distance_l = self.distance_left
         starting_distance_r = self.distance_right
         current_distance = 0
-        
+
         msg = WheelsCmdStamped()
         msg.header.stamp = rospy.get_rostime()
-        
+
         msg.vel_left = -self.turn_speed
-        msg.vel_right = self.turn_speed
-        
+        msg.vel_right = self.turn_speed + self.slow_down # csc22906
+
         while current_distance < total_distance:
+            rospy.loginfo("left_turn current_distance " + str(current_distance))
             current_distance = (abs(starting_distance_l - self.distance_left) + abs(starting_distance_r - self.distance_right))/2
             self.pub_wheel_commands.publish(msg)
         self.stop()
-        
+
     def right_turn(self, angle):
         total_distance = self.rot_dist(angle)
-        
+
         starting_distance_l = self.distance_left
         starting_distance_r = self.distance_right
         current_distance = 0
-        
+
         msg = WheelsCmdStamped()
         msg.header.stamp = rospy.get_rostime()
-        
+
         msg.vel_left = self.turn_speed
         msg.vel_right = -self.turn_speed
-        
+
         while current_distance < total_distance:
+            rospy.loginfo("right_turn current_distance " + str(current_distance))
             current_distance = (abs(starting_distance_l - self.distance_left) + abs(starting_distance_r - self.distance_right))/2
-            self.pub_wheel_commands.publish(msg)  
+            self.pub_wheel_commands.publish(msg)
         self.stop()
-        
+
+    def first_right_turn(self, angle):
+        total_distance = self.rot_dist(angle)
+
+        starting_distance_l = 0
+        starting_distance_r = 0
+        current_distance = 0
+
+        msg = WheelsCmdStamped()
+        msg.header.stamp = rospy.get_rostime()
+
+        msg.vel_left = self.turn_speed
+        msg.vel_right = -self.turn_speed + self.slow_down # csc22906
+        self.pub_wheel_commands.publish(msg)
+
+        while current_distance < total_distance:
+            rospy.loginfo("first_right_turn current_distance " + str(current_distance))
+            current_distance = (abs(starting_distance_l - self.distance_left) + abs(starting_distance_r - self.distance_right))/2
+            self.pub_wheel_commands.publish(msg)
+        self.stop()
+
 
     def stop(self):
         msg = WheelsCmdStamped()
@@ -141,17 +165,17 @@ class ControllerNode(DTROS):
         msg.vel_left = 0.0
         msg.vel_right = 0.0
         self.pub_wheel_commands.publish(msg)
-        
+
     def publish_leds(self, color):
         try:
             response = self.led_service(String(color))
             rospy.loginfo(response)
         except Exception as e:
             rospy.loginfo("Failure to change LED pattern:" + str(e))
-            
+
     def cb_distance_left(self, msg):
         self.distance_left = msg.data
-        
+
     def cb_distance_right(self, msg):
         self.distance_right = msg.data
 
@@ -162,3 +186,5 @@ if __name__ == '__main__':
     node.run()
     # keep spinning
     # rospy.spin()
+    reason = "Program over. Exiting."
+    rospy.signal_shutdown(reason)

packages/demo_package/src/odometry_node.py
@@ -24,7 +24,7 @@ class OdometryNode(DTROS):
         self.sub_encoder_ticks_left = rospy.Subscriber(f'/{self.veh_name}/left_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data, callback_args='left')
         self.sub_encoder_ticks_right = rospy.Subscriber(f'/{self.veh_name}/right_wheel_encoder_node/tick', WheelEncoderStamped, self.cb_encoder_data, callback_args='right')
         self.sub_executed_commands = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd_executed', WheelsCmdStamped, self.cb_executed_commands)
-        
+
          # Subscribing to the wheels_cmd
         # self.sub_wheels_cmd = rospy.Subscriber(f'/{self.veh_name}/wheels_driver_node/wheels_cmd', WheelsCmdStamped, self.cb_received_commands)
 
@@ -32,22 +32,22 @@ class OdometryNode(DTROS):
         self.pub_integrated_distance_left = rospy.Publisher(f'/{self.veh_name}/odometry_node/integrated_distance_left', Float32, queue_size=10)
         self.pub_integrated_distance_right = rospy.Publisher(f'/{self.veh_name}/odometry_node/integrated_distance_right', Float32, queue_size=10)
         self.pub_rotational_vel = rospy.Publisher(f'/{self.veh_name}/odometry_node/rotational_vel', Float32, queue_size=10)
-        
+
         # attributes for computing wheel distance
         self.prev_left = 0	# number of ticks on left wheel at last computation time
         self.prev_right = 0	# number of ticks on right wheel at last computation time
-        
+
         self.first_message_left = True	# set to false after first computation
         self.first_message_right = True	# set to false after first computation
         self.initial_left = 0	# the number of ticks on left wheel at first computation
         self.initial_right = 0 # the number of ticks on right wheel at first computation
-        
+
         self.left_distance = 0	# total distance traveled by left wheel in meters
         self.right_distance = 0	# total distance traveled by right wheel in meters
-        
+
         self.vel_left = 0	# the velocity of left wheel in the range [-1.0, 1.0]
         self.vel_right = 0	# the velocity of right wheel in the range [-1.0, 1.0]
-        
+
         # attribtues for computing rotational velocity
         # speed of wheels
         self.l = 0.045 # meters between the center of the wheel and the robot rotation
@@ -55,7 +55,7 @@ class OdometryNode(DTROS):
         self.last_dist_left = 0
         self.last_dist_right = 0
         self.theta = 90
-        
+
 
         self.log("Initialized")
 
@@ -65,7 +65,7 @@ class OdometryNode(DTROS):
         # retreive ticks and resolution from message
         ticks = msg.data
         resolution = msg.resolution
-        
+
         # Check if it's the first received message and store initial encoder value
         if wheel == 'left' and self.first_message_left == True:
             self.first_message_left = False
@@ -73,21 +73,21 @@ class OdometryNode(DTROS):
         if wheel == 'right' and self.first_message_right ==True:
             self.first_message_right = False
             self.initial_right = ticks
-        
+
         # Compute total distance traveled by the left wheel
         if wheel == 'left':
             rel_ticks = ticks - self.initial_left
             diff_ticks = np.abs(rel_ticks - self.prev_left)
             dist = (2 * np.pi * self._radius * diff_ticks / resolution)
-            
+
             # Accumulate distance and publish it
             if self.vel_left >= 0:
             	self.left_distance += dist
             else:
                 self.left_distance -= dist
-                
+
             self.pub_integrated_distance_left.publish(self.left_distance)
-            
+
             self.prev_left = rel_ticks
 
         # Compute total distance traveled by the right wheel
@@ -96,17 +96,17 @@ class OdometryNode(DTROS):
             # rospy.loginfo("ticks " + str(ticks))
             diff_ticks = np.abs(rel_ticks - self.prev_right)
             dist = (2 * np.pi * self._radius * diff_ticks / resolution)
-            
+
             # Accumulate distance and publish it
             if self.vel_right >= 0:
             	self.right_distance += dist
             else:
                 self.right_distance -= dist
-                
+
             self.pub_integrated_distance_right.publish(self.right_distance)
-            
+
             self.prev_right = rel_ticks
-            
+
         # compute current rotational velocity
 
     def cb_executed_commands(self, msg):
@@ -116,29 +116,29 @@ class OdometryNode(DTROS):
         self.vel_left = msg.vel_left
         self.vel_right = msg.vel_right
         #rospy.loginfo("Executed: left wheel: "+ str(msg.vel_left) + " right wheel: "+ str(msg.vel_right))
-    
+
     def cb_received_commands(self, msg):
         rospy.loginfo("Sent: left wheel: "+ str(msg.vel_left) + " right wheel: "+ str(msg.vel_right))
-        
+
     def run(self):
         rate = rospy.Rate(1) # 1Hz
         while not rospy.is_shutdown():
             left_distance_t0 = self.left_distance
             right_distance_t0 = self.right_distance
-            
+
             rate.sleep()
-            
+
             left_distance_t1 = self.left_distance
             right_distance_t1 = self.right_distance
-        
+
         w_l = (self._radius * self.vel_left) / (2 * self.l)
         w_r = (self._radius * self.vel_right) / (2 * self.l)
         w_delta = w_r - w_l
-        
+
 
 if __name__ == '__main__':
     node = OdometryNode(node_name='odometry_node')
     # Run the node
     node.run()
     # Keep it spinning to keep the node alive
-    rospy.spin()
+    # rospy.spin()